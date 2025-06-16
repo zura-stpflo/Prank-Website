adab5b09d55fd6c30671ae0f2dde38b744b44c68
Zura Hosting <rasyaml1131@gmail.com>
2025-06-16 22:14:41 +0700

    Update prank.js

--- prank.js
+++ prank.js
@@ -1,6 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-  alert("⚠️ Virus detected!");
-  setInterval(() => {
-    document.body.style.backgroundColor = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
-  }, 300);
-});
+window.onload = function () {
+  const video = document.getElementById('prank-video');
+  const audio = document.getElementById('prank-audio');
+
+  // Ganti link di bawah ini dengan link video prank kamu (mp4)
+  video.src = "https://www.w3schools.com/html/mov_bbb.mp4";
+  video.load();
+  audio.load();
+  audio.play();
+};
