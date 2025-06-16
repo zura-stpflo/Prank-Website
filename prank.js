3dc2bd440244aa1f094d2c1debf031471ff45d51
Zura Hosting <rasyaml1131@gmail.com>
2025-06-17 05:18:59 +0700

    Update prank.js

--- prank.js
+++ prank.js
@@ -3,7 +3,7 @@ window.onload = function () {
   const audio = document.getElementById('prank-audio');
 
   // Ganti link di bawah ini dengan link video prank kamu (mp4)
-  video.src = "https://www.w3schools.com/html/mov_bbb.mp4";
+  video.src = "https://files.catbox.moe/x24fxy.mp4";
   video.load();
   audio.load();
   audio.play();
